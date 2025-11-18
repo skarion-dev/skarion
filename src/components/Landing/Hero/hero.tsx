96231e7ba7730522f45495a3276272a4a0c9e9f4
Tawkir Arifin <98356837+synyster100@users.noreply.github.com>
2025-11-18 12:07:14 +0600

    glass and hero background

--- src/components/Landing/Hero/hero.tsx
+++ src/components/Landing/Hero/hero.tsx
@@ -1,6 +1,10 @@
+import Image from "next/image";
+
 export default function Hero(){
     return (
-        <div className="w-full h-[100vh] bg-[#ffffff]">
+        <div className="relative w-full h-[100vh]">
+            <Image src="/bgimage.jpg" alt="Background" fill priority className="object-cover" />
+            <div className="absolute inset-0 bg-black/20" />
         </div>
     )
 }
