1b989af595b538d8231b663bb2831266e23dfc46
Rianul Amin Rian <rianulamin.r@Rianuls-MacBook-Air.local>
2025-11-18 12:19:38 +0600

    hero section layout updated

--- src/components/Landing/Hero/hero.tsx
+++ src/components/Landing/Hero/hero.tsx
@@ -1,6 +1,33 @@
-export default function Hero(){
+export default function Hero() {
     return (
-        <div className="w-full h-[100vh] bg-[#ffffff]">
+        <div className="w-full bg-[#ffffff] py-10 px-12">
+            <div className="flex flex-row items-start justify-between space-x-8">
+                <div className="text-[#000000] text-[64px] w-[60%] leading-[1.3] text-left">
+                    Advancing Careers Through<br />Specialized Training
+                </div>
+                <div className="w-[35%]">
+                    <div className="bg-[#ff686b] rounded-[14px] h-[180px]"></div>
+                    <div className="text-[#787777] text-[18px] font-[500] pt-8 text-left">
+                        Skarion helps individuals gain the skills and experience needed to secure high-demand jobs through specialized, hands-on training.                    
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex flex-row items-start justify-between space-x-8 pt-12">
+            <div className="flex flex-col gap-8 w-[62%] h-[600px]">
+                <div className="bg-[#122461] rounded-[14px] flex-1"></div>
+                <div className="flex flex-row items-start justify-between flex-1 space-x-8">
+                    <div className="bg-[#122461] rounded-[14px] w-[70%] h-full"></div>
+                    <div className="bg-[#122461] rounded-[14px] w-[30%] h-full"></div>
+                </div>
+            </div>
+
+            <div className="flex flex-col gap-8 bg-[#122461] rounded-[14px] w-[35%] h-[600px]">
+            
+            </div>
+        </div>
+
+
         </div>
     )
 }
