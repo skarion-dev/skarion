e413ad89902bb0c2448bc8f880b8727e85a4faf0
Tawkir Arifin <98356837+synyster100@users.noreply.github.com>
2025-11-25 19:01:29 +0600

    loop edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Footer from "@/components/Footer";
-import Hero from "@/components/Landing/Hero/Hero";
+import Hero from "@/components/Landing/Hero/hero";
+import About from "@/components/Landing/About/about"
 import SmoothScroll from "@/components/SmoothScroll";
 
 export default function Home() {
   return (
     <SmoothScroll>
       <Hero />
+      <About />
       <Footer />
     </SmoothScroll>
   );
--- src/components/LogoCarousel.tsx
+++ src/components/LogoCarousel.tsx
@@ -27,23 +27,29 @@ export default function LogoCarousel() {
       ease: "linear",
       repeat: Infinity,
       repeatType: "loop",
-      repeatDelay: 0,
+      repeatDelay: 0.0,
+
     });
 
     return () => controls.stop();
   }, [xTranslation, width]);
 
   return (
-    <div className="flex relative overflow-hidden before:absolute before:left-0 before:top-0 before:z-10 before:h-full before:w-10 before:bg-gradient-to-r before:from-white before:to-transparent before:content-[''] after:absolute after:right-0 after:top-0 after:h-full after:w-10 after:bg-gradient-to-l after:from-white after:to-transparent after:content-['']">
+    <div className="flex relative overflow-hidden before:absolute before:left-0 before:top-0 before:z-10 before:h-full before:w-10 before:bg-gradient-to-r before:from-white before:to-transparent before:content-[''] after:absolute after:right-0 after:top-0 after:h-full after:w-10 after:bg-gradient-to-l after:from-white after:to-transparetent-['']">
     <motion.div
-      className="flex gap-8"
+      className="flex gap-8 width-max-c[...logos, ...onten]t"
+      style={{animation: 'infinite-scroll 100s linear infinite'}}
       ref={ref}
       style={{ x: xTranslation }}
     >
       {[...logos, ...logos].map((item, idx) => (
-        <Image key={idx} src={item} alt={`logo-${idx}`} width={60} height={60} />
+        <Image key={idx} src={item} alt={`go-${idx}`} width={60} height={60} />
+      ))}
+      {[...logos, ...logos].map((item, idx) => (
+        <Image key={idx} src={item} alt={`go-${idx}`} width={60} height={60} />
       ))}
     </motion.div>
+    
     </div>
   );
 }
