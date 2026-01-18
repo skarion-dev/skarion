b8e7ed8301f38c8ddccf956c86f48dae3a984ca6
Rianul Amin Rian <rianulamin.r@Rianuls-MacBook-Air.local>
2026-01-19 04:24:26 +0600

    bing verify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
       <html lang="en">
         <head>
           <link rel="icon" type="image/x-icon" href="https://skarion.com/skarion.ico"/>
+          <meta name="msvalidate.01" content="C6BC22315AAA3905A487DA8CF4F013C9" />
         </head>
         <body
           className={`${inter.variable} font-sans antialiased overflow-x-hidden`}
