b2b9257d8121c60ca3b798c4ba1d5da69d5073cb
Rianul Amin Rian <rianulamin.r@Rianuls-MacBook-Air.local>
2026-04-06 01:41:55 +0600

    dashboard auth

--- src/auth.ts
+++ src/auth.ts
@@ -33,6 +33,8 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
           name: data.name,
           email: data.email,
           role: data.role,
+          roles: data.roles ?? [],
+          permissions: data.permissions ?? [],
           image: data.image,
           lastLogin: data.lastLogin,
           accessToken: data.accessToken,
@@ -64,6 +66,8 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
           name: user.name,
           email: user.email,
           role: user.role,
+          roles: user.roles ?? [],
+          permissions: user.permissions ?? [],
           image: user.image,
           lastLogin: user.lastLogin,
           accessToken: user.accessToken,
@@ -80,6 +84,8 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       if (user) {
         token.id = String(user.id);
         token.role = user.role;
+        token.roles = user.roles ?? [];
+        token.permissions = user.permissions ?? [];
         token.image = user.image;
         token.lastLogin = user.lastLogin;
         token.email = user.email;
@@ -91,6 +97,8 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       if (session.user) {
         session.user.id = token.id as string;
         session.user.role = token.role as string;
+        session.user.roles = (token.roles ?? []) as string[];
+        session.user.permissions = (token.permissions ?? []) as string[];
         session.user.image = token.image as string;
         session.user.lastLogin = token.lastLogin as string;
         session.user.email = token.email as string;
